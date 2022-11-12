c6ac4a699b2f46278e26573b5384fd311a59ada9
Andrew Wand <andrewwands@gmail.com>
2022-11-12 15:45:59 -0600

    added some components

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="header-container">
+        <div className="header-inner">
+          <div className="header-content">
+            <h1>Movie Night</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
