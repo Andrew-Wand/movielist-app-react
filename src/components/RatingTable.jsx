32dc195581d4f59364368e07ccb866b37faf6398
Andrew Wand <andrewwands@gmail.com>
2022-11-19 17:45:57 -0600

    can choose movie with drop down

--- src/components/RatingTable.jsx
+++ src/components/RatingTable.jsx
@@ -1,15 +1,44 @@
-import React, { useEffect } from "react";
+import { useState } from "react";
 
 function RatingTable({ movieList }) {
+  const [movieTitle, setMovieTitle] = useState("Select a movie");
+
+  let handleMovieChange = (e) => {
+    setMovieTitle(e.target.value);
+  };
+
   return (
     <div className="rating-table-container">
       <div className="rating-table">
-        <div className="test">
+        <table>
+          <caption>Movie Night</caption>
+          <thead>
+            <tr>
+              <th>Movie Title </th>
+              <th>Date Watched</th>
+              <th>Rating</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>(Movie title) {movieTitle}</td>
+              <td>(Date Watched)</td>
+              <td>(Rating)</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div className="rating-modal-container">
+        <div className="rating-modal-content">
           <label htmlFor="movies">Choose a movie:</label>
-          <select name="movies" id="movies">
+          <select name="movies" id="movies" onChange={handleMovieChange}>
             <option value="">Select a movie</option>
             {movieList.map((movie) => (
-              <option value={movie.name}>{movie.name}</option>
+              <option key={movie.name} value={movie.name}>
+                {movie.name}
+              </option>
             ))}
           </select>
         </div>
