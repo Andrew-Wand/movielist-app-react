dc75756082ea089d300479d1e5d12773ec7e2793
Andrew Wand <andrewwands@gmail.com>
2022-11-19 18:12:43 -0600

    html for rating table form

--- src/components/RatingTable.jsx
+++ src/components/RatingTable.jsx
@@ -3,9 +3,18 @@ import { useState } from "react";
 function RatingTable({ movieList }) {
   const [movieTitle, setMovieTitle] = useState("Select a movie");
 
+  const [formTitle, setFormTitle] = useState([]);
+
   let handleMovieChange = (e) => {
     setMovieTitle(e.target.value);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormTitle([{ title: movieTitle }]);
+  };
+
+  console.log(formTitle);
 
   return (
     <div className="rating-table-container">
@@ -22,7 +31,10 @@ function RatingTable({ movieList }) {
 
           <tbody>
             <tr>
-              <td>(Movie title) {movieTitle}</td>
+              {formTitle.map((movie) => (
+                <td>{movie.title}</td>
+              ))}
+              {/* <td>(Movie title) {movieTitle}</td> */}
               <td>(Date Watched)</td>
               <td>(Rating)</td>
             </tr>
@@ -31,16 +43,35 @@ function RatingTable({ movieList }) {
       </div>
 
       <div className="rating-modal-container">
+        <div className="rating-modal-header">
+          <div className="rating-title">
+            <h2>Add your movie</h2>
+          </div>
+        </div>
         <div className="rating-modal-content">
-          <label htmlFor="movies">Choose a movie:</label>
-          <select name="movies" id="movies" onChange={handleMovieChange}>
-            <option value="">Select a movie</option>
-            {movieList.map((movie) => (
-              <option key={movie.name} value={movie.name}>
-                {movie.name}
-              </option>
-            ))}
-          </select>
+          <div className="modal-form">
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="movies">Choose a movie:</label>
+              <select name="movies" id="movies" onChange={handleMovieChange}>
+                <option value="">Select a movie</option>
+                {movieList.map((movie) => (
+                  <option key={movie.name} value={movie.name}>
+                    {movie.name}
+                  </option>
+                ))}
+              </select>
+              <label htmlFor="date">Date:</label>
+              <input type="date" />
+              <input type="submit" value="Submit" />
+
+              <label htmlFor="rating">Rating:</label>
+              <input type="text" />
+            </form>
+          </div>
+        </div>
+
+        <div className="rating-modal-footer">
+          <button>Close</button>
         </div>
       </div>
     </div>
