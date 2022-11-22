d3a076589f7ee2b974507288153a8db4289dd7ac
Andrew Wand <andrewwands@gmail.com>
2022-11-22 11:29:16 -0600

    local storage for movie list

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import RatingTable from "./components/RatingTable";
 import MovieList from "./components/MovieList";
 import Spin from "./components/Spin";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const movies = [
     { id: 1, name: "Derp" },
     { id: 2, name: "Merp" },
   ];
-  const [movieList, setMovieList] = useState(movies);
+
+  const localMovie = localStorage.getItem("movieList")
+    ? JSON.parse(localStorage.getItem("movieList"))
+    : [];
+
+  const [movieList, setMovieList] = useState(localMovie);
+
+  useEffect(() => {
+    const json = JSON.stringify(movieList);
+    window.localStorage.setItem("movieList", json);
+  }, [movieList]);
 
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-
         <Routes>
           <Route path="/" element={<RatingTable movieList={movieList} />} />
           <Route
